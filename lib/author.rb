43cd8c1e45792bad2a83406fb1453a896fbf3010
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-02 17:51:19 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,18 @@
+class Author 
+  
+  attr_accessor :name, :post 
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def posts
+    Post.all
+  end 
+  
+  def add_post(post)
+    post.author == self 
+  end
+  
+  
+end 
